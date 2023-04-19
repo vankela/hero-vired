b9f1f51e6326af79bb683269f9b79d2177ad4da4
vankela <130962867+vankela@users.noreply.github.com>
2023-04-19 10:33:21 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,5 @@
 leap_year=int(input("Enter the value: "))
 if(leap_year%400==0 or(leap_year%4==0 and leap_year%100!=0)):
-      print("Leap_Year")
+      print("Leap_Year");
  else:
-      print("Not a leap year")
+      print("Not a leap year");
