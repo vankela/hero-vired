dd740dd902beff1df33ae0af8269cde130f20e6c
vankela <130962867+vankela@users.noreply.github.com>
2023-04-27 18:23:37 +0530

    Add files via upload

---
+++ query1.sql
@@ -0,0 +1,10 @@
+-- Creating data base and its Name.
+-- create database rgm 
+
+-- create  table student(
+-- student_id int primary key not null,
+-- student_name varchar(30) not null,
+-- student_contact int not null,
+-- student_dept varchar(25) not null
+-- );
+select * from student;
