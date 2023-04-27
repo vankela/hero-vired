04453f6a9a62ff30db0989ddb8468ceba639555f
vankela <130962867+vankela@users.noreply.github.com>
2023-04-27 18:17:40 +0530

    Add files via upload

---
+++ query2.sql
@@ -0,0 +1,17 @@
+create database Herovired;
+create table RGM(name varchar(40)
+ primary key not null,
+ college varchar(40) not null,
+ session varchar(40) not null,
+ contact_number int not null,
+ address varchar(30) not null
+);
+
+
+select * from RGM
+INSERT INTO RGM(name,college,session,contact_number,address)
+ VALUES ("sai","rgmcet","python",456789,"badvel");
+ INSERT INTO RGM VALUES("lakshmi","srcet","sql",34567,"nandyal"),("preethi","prcet","fullstack",345678,"kadapa"),("radhika","svrcet","java",987654,"kurnool"),
+ ("loki","viswasanthi","AI",876543,"anathapuru");
+ 
+ 
