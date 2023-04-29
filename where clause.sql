b0550823da8b7253a79814a060881f062aedbc09
vankela <130962867+vankela@users.noreply.github.com>
2023-04-29 18:50:39 +0530

    sql

---
+++ where clause.sql
@@ -0,0 +1,28 @@
+-- --- find the all the rows section=java
+SELECT * FROM rgm where session="java";
+
+SELECT name,college,session from rgm where session="java";
+SELECT * FROM rgm;
+
+SELECT * FROM rgm where session="sql" AND address="nandyal";
+
+SELECT * FROM rgm where address="kurnool";
+
+SELECT * From rgm where name LIKE  "l%";
+
+SELECT * From rgm where name LIKE  "sa%";
+SELECT * From rgm where name LIKE  "%i";
+
+SELECT * From rgm where name LIKE  "l%";
+
+Select * from rgm where name like "_____k%";
+
+SELECT * From rgm where name NOT LIKE  "l%";
+SELECT * From rgm where name NOT LIKE  "%i";
+
+
+
+
+
+
+
