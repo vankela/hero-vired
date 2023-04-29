0e21200bfd388a312117cf2ae130990494ba81cc
vankela <130962867+vankela@users.noreply.github.com>
2023-04-29 18:53:41 +0530

    28-04-23//practices problem

--- saleperson.sql
+++ saleperson.sql
@@ -4,4 +4,4 @@
    INSERT Into salesperson values(5001,"james hoog","new york",0.15),(5002,"Nail Knite","parsis",0.13),
    (5005,"pit Alex","London",0.11),(5006,"Mc Lyon","parsis",0.14);
    Insert into salesperson values(5007,"Paul adam","Rome",0.13),(5003,"Lauson Hen","san Jose",0.12);
-   select distinct name,commission from salesperson
+   select distinct name,commission from salesperson;
